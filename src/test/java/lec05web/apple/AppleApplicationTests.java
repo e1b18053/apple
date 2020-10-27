7016f28eb2dde0be98cd28d2dcc1c345dfd6c4e9
e1b18053 <e1b18053@oit.ac.jp>
2020-10-27 16:29:24 +0900

    ファーストコミット

---
+++ src/test/java/lec05web/apple/AppleApplicationTests.java
@@ -0,0 +1,13 @@
+package lec05web.apple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
